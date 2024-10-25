3f3e61b3174fd4ef13a26d34b4e7661517b09e3b
Sharline M. Payne Britton <111597150+Sharpy26@users.noreply.github.com>
2024-10-24 21:07:42 -0500

    Add files via upload

---
+++ PARCIALES/PARCIAL_3/BD.php
@@ -0,0 +1,24 @@
+<?php
+$usuarios = [
+    'estudiante' => [
+        'usuario1' => 'contrasena_usuario',
+        'usuario2' => 'password_user'
+    ],
+    'profesor' => [
+        'profesor1' => 'contrasena_profesor',
+        'profesor2' => 'password_prof'
+    ]
+];
+
+$calificaciones = [
+    'usuario1' => [
+        'desarrollo de software' => 'A',
+        'ingeneria de software'    => 'D'
+       
+    ],
+    'usuario2' => [
+       'desarrollo de software' => 'C',
+        'ingeneria de software'   => 'B'
+    ]
+];
+?>
